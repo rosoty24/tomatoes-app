30e32c52a3400aecd8ff3a2790e2c33b50653c31
Theara WATT <#>
2016-01-22 10:32:50 +0700

    Crud Review

--- client/admin/controller/review.js
+++ client/admin/controller/review.js
@@ -15,22 +15,47 @@ Template.addreview.events({
 			date:date
 		}
 		Meteor.call("insertReview",obj);
-	}
+	},
+	"click #remove":function(e){
+        e.preventDefault();
+        //alert('Remove Project!!!');
+        var id = this._id;
+        if (confirm("Are you sure you want to delete this?")) {
+            Meteor.call("deleteReview",id);
+        }
+    }
 });
 Template.addreview.helpers({
 	getAllReview:function(){
 		return review.find();
 	},
 	getProduct:function(){
-		return products.find();
+		return products.find({});
 	}
 });
-Template.review.helpers({
-	getAllReview:function(){
-		return review.find();
-	},
+Template.updateReview.events({
+	"click #update":function(e,tlp){
+		e.preventDefault();
+		var id = this._id;
+		var text = tlp.$('#text').val();
+		var productiId= tlp.$('#products').val();
+		var userId = Meteor.userId();
+		var date = new Date();
+		var type = "Products Type";
+		alert('Can Get :'+text+productiId+userId);
+		var obj={
+			text:text,
+			productId:productiId,
+			userId:Meteor.userId(),
+			type:type,
+			date:date
+		}
+		Meteor.call("insertReview",id,obj);
+		Router.go('/admin/addreview')
+	}
+});
+Template.updateReview.helpers({
 	getProduct:function(){
-		var id = this.productiId;
-		return review.findOne({_id:id});
+		return products.find({});
 	}
 });
--- server/review.js
+++ server/review.js
@@ -1,5 +1,11 @@
 Meteor.methods({
 	insertReview: function(obj) {
   		review.insert(obj);
+  	},
+  	deleteReview: function (id) {
+      return review.remove(id);
+   },
+   insertReview: function(id,obj) {
+  		review.update({_id:id},{$set:obj});
   	}
 });
