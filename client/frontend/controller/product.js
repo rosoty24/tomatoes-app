0964050d81ebf72583293d2b8b819744ee9ffcca
socheata.meach <socheata.meach2015@gmail.com>
2016-01-23 08:31:39 +0700

    comments

--- client/frontend/controller/product.js
+++ client/frontend/controller/product.js
@@ -81,3 +81,65 @@ Template.disProduct.events({
         });
     }
 });
+
+// comment and review
+Template.productdetails.events({
+    "click #submit": function(e,tlp){
+        e.preventDefault();
+        var id = this._id;
+        alert("id "+id);
+        var date = new Date();
+        var text = tlp.$('#comment').val();
+        var userId = Meteor.userId();
+        var type = "My type of products";
+        var object={
+            comments:
+                {
+                     text:text, 
+                     date:date, 
+                     userId:Meteor.userId(), 
+                     type:type
+                }
+        }
+        if(userId){
+            Meteor.call('updatePro',id,object);
+        }else{
+            alert("Please login");
+        }
+        
+        //post.update(this._id,object);
+        
+    },
+    // "click .clickHere":function(e){
+    //     $(e.currentTarget).next(".hideCom").toggle();
+        
+    // }
+});
+Template.productdetails.helpers({
+    getPostCom:function(){
+        var id = this._id;
+        var result= products.find({_id:id});
+        return result;
+    },
+    getReview:function(){
+        var id = this._id;
+        console.log("MY ID IS "+id);
+        var result = review.find({productId:id});
+        console.log(" MY RESTLT IS "+result);
+        return result;
+    },
+    getUserReview:function(userId){
+        var result = users.findOne({_id:userId});
+        return result.profile.firstname;
+    },
+    getImage: function(){
+        var image = this.img;
+        var img = images.findOne({_id:image});
+        if(img){
+            console.log(img.copies.images.key);
+            return img.copies.images.key;
+        }else{
+            return;
+        }
+    }
+});
